dc23a674638d7cc9c7fd7c94cda200fa0add6773
Jake Barnes <jake@Jakes-MBP.broadband>
2018-12-27 22:28:41 +0000

    functional programming 1

---
+++ src/main/scala/lectures/part3fp/WhatsAFunction.scala
@@ -0,0 +1,56 @@
+package lectures.part3fp
+
+/**
+  * Created by Jake Barnes on 27/12/2018
+  */
+object WhatsAFunction extends App {
+  // DREAM: use functions as first class elements
+  // problem: OOP
+
+  val doubler = new MyFunction[Int, Int] {
+    override def apply(element: Int): Int = element * 2
+  }
+
+  println(doubler(3))
+
+  // Function types = Function1[A, B]
+  val stringToIntConverter = new Function[String, Int] {
+    override def apply(string: String): Int = string.toInt
+  }
+
+  println(stringToIntConverter("7") + 3)
+
+
+  //NOTE syntactic sugar: Function2[Int, Int, Int] == (Int, Int) => Int)
+  val adder: ((Int, Int) => Int) = new Function2[Int, Int, Int] {
+    override def apply(a: Int, b: Int): Int = a + b
+  }
+
+  //Function types = Function2[A, B, R] === (A, B) => R
+
+  // ALL SCALA FUNCTIONS ARE OBJECTS
+
+  /*
+      1. a function which takes two strings and concatenates them
+      2. transform the MyTransformer and MyPredicate into function types
+      3. define a function which takes an int and returns another function which takes an int and returns an int
+         - whats the type of this function
+         - how to implement this function
+   */
+
+  val stringConcatenator: (String, String) => String = new Function2[String, String, String] {
+    override def apply(a: String, b: String): String = a + b
+  }
+
+  println(stringConcatenator("Hello", "World"))
+
+
+
+
+
+}
+
+
+trait MyFunction[A, B] {
+  def apply(element: A): B
+}
